079273487f63219576f721151b4097d49bcc3a8d
muhammadganiev <mukhammad.ganiev@mail.ru>
2025-05-12 15:17:27 +0400

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div id="part0" className=" w-screen h-screen bg-black">
         <Spline
           className="fixed top-0 left-0 right-0 bottom-0 z-10"
-          scene="https://draft.spline.design/J5nsmkDRgW4gMCTS/scene.splinecode"
+          scene="https://draft.spline.design/uLhRZxO27JMQP9ic/scene.splinecode"
         />
       </div>
     </>
