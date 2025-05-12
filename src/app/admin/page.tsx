1a35598177e35963bbc1cf0b063d55f1069cbd18
muhammadganiev <mukhammad.ganiev@mail.ru>
2025-05-12 13:55:55 +0400

    fixed 1 line for dep

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { AppSidebar } from "@components/app-sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 import DataTable from "@components/data-table";
 import { SiteHeader } from "@components";
 import { SidebarInset, SidebarProvider } from "@ui";
